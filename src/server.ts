84fd403e1cdf660cf86717b3019e5627f4701c06
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-09-17 16:15:04 -0300

    config: server.js to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import app from './app';
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
