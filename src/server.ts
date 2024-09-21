f4e84c7071e368f419c44bd0076666dd928aac7a
Gabriel Siqueira <gabrielss2406@gmail.com>
2024-09-21 16:41:21 -0300

    feat: cow number on painel

--- src/server.ts
+++ src/server.ts
@@ -7,29 +7,20 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-// Cria um servidor HTTP
 const server = http.createServer(app);
 
-// Configura o Socket.io
 const io = new Server(server);
 
-// Quando um cliente se conecta via Socket.io
 io.on('connection', (socket) => {
     console.log('Cliente conectado ao sistema de renderização');
 });
 
-// Rota do Webhook para receber dados da balança
 app.post('/webhook', (req, res) => {
-    const { weight } = req.body;
-    console.log(`Webhook recebeu o peso: ${weight}`);
-
-    // Envia o peso para o cliente via Socket.io
-    io.emit('updateWeight', { weight });
-
+    const { weight, number } = req.body;
+    io.emit('updateWeight', { weight, number });
     res.status(200).send('Peso recebido pelo sistema de renderização.');
 });
 
-// Inicia o servidor
 server.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
